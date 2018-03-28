b28c03ff53c48266b721c7ba8ed5ad63b8727500
Saurabh <1saurabh.desai@gmail.com>
2018-03-28 23:09:37 +0530

    5 Kyu problem related to an array

---
+++ js/5 Kyu - Moving Zeros To The End.js
@@ -0,0 +1,27 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+*/
+
+var moveZeros = function (arr) {
+  
+  let parsedArr = arr.reduce((a,b,i,arr) => {
+    
+    if (b === 0) {
+      a.zeroArr.push(b);
+    } else {
+      a.arr.push(b)
+    }
+    
+    return a;
+    
+  }, {
+    arr:[],
+    zeroArr: []
+  } );
+  
+  return [...parsedArr.arr, ...parsedArr.zeroArr];
+  
+}
