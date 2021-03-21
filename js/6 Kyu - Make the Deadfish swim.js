2c4d7b443db929323fe8978ea3502eb9be070f42
Saurabh Ds <1saurabh.desai@gmail.com>
2021-03-21 23:30:19 +0530

    Kyu 6 JS and TS problems related to string / array

---
+++ js/6 Kyu - Make the Deadfish swim.js
@@ -0,0 +1,58 @@
+/*
+
+
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso") => [8, 64]
+
+
+*/
+
+
+
+function parse(data) {
+  
+  if (!data) 
+    return null;
+  
+  const commands = data.split(''), op = [];
+  
+  let val = 0;
+  for (let i = 0, n = commands.length; i<n; i++) {
+    
+    switch (commands[i]) {
+      case 'i' : {
+        ++val;
+        break;
+      }
+      
+      case 's' : {
+        val = Math.pow(val, 2)
+        break;
+      }
+        
+      case 'd' : {
+        --val;
+        break;
+      }
+        
+      case 'o' : {
+        op.push(val);
+        
+        break;
+      }   
+        
+    }
+    
+  }
+  
+  return op;
+}
